a768f21e8774c7bb4efa6e35c50b0c8496dca5b3
Lordede <lukas.edm@web.de>
2022-03-04 17:02:41 +0100

    Added changes until video nr. 8

--- Frontend/README.md
+++ Frontend/README.md
@@ -1,2 +1,2 @@
-- Weiter bei Video Nr. 6
-https://www.youtube.com/watch?v=gj6f-asv3wM&list=PL_NVFNExoAxclqXo9fLAeP0G2Qp56Fu8C&index=6
+- Weiter bei Video Nr. 9
+https://www.youtube.com/watch?v=Y7Blm87AfW8&list=PL_NVFNExoAxclqXo9fLAeP0G2Qp56Fu8C&index=9
--- Frontend/src/app/property/property-list/property-list.component.ts
+++ Frontend/src/app/property/property-list/property-list.component.ts
@@ -7,6 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PropertyListComponent implements OnInit {
 
+  propertys: Array<any> =[
+    {
+    "Id": 1,
+    "Name": "Birla House",
+    "Type": "House",
+    "Price": 12000
+    },
+    {
+    "Id": 2,
+    "Name": "Bumbsvilla",
+    "Type": "Villa",
+    "Price": 13000
+    },
+    {
+    "Id": 3,
+    "Name": "Drecksloch",
+    "Type": "Flat",
+    "Price": 14500
+    },
+    {
+    "Id": 4,
+    "Name": "Spielwiese",
+    "Type": "House",
+    "Price": 16000
+    },
+    {
+    "Id": 5,
+    "Name": "Shithole",
+    "Type": "Flat",
+    "Price": 18000
+    },
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
