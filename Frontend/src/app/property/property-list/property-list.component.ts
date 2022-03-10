3af45c816d75183237304fab06f159d45c07d484
Lordede <lukas.edm@web.de>
2022-03-10 14:34:08 +0100

    changes before HttpClientModule

--- Frontend/src/app/property/property-list/property-list.component.ts
+++ Frontend/src/app/property/property-list/property-list.component.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,42 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PropertyListComponent implements OnInit {
 
-  propertys: Array<any> =[
-    {
-    "Id": 1,
-    "Name": "Birla House",
-    "Type": "House",
-    "Price": 12000
-    },
-    {
-    "Id": 2,
-    "Name": "Bumbsvilla",
-    "Type": "Villa",
-    "Price": 13000
-    },
-    {
-    "Id": 3,
-    "Name": "Drecksloch",
-    "Type": "Flat",
-    "Price": 14500
-    },
-    {
-    "Id": 4,
-    "Name": "Spielwiese",
-    "Type": "House",
-    "Price": 16000
-    },
-    {
-    "Id": 5,
-    "Name": "Shithole",
-    "Type": "Flat",
-    "Price": 18000
-    },
-  ]
+  properties!: Array<any>;
 
-  constructor() { }
+  constructor(private http:HttpClientModule) { }
 
   ngOnInit(): void {
+
   }
 
 }
