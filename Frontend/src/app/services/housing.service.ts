2d7a7ef78f57776266283132e58b7d8d6a0b4b65
Lordede <lukas.edm@web.de>
2022-03-14 14:51:12 +0100

    changes from v11

--- Frontend/src/app/services/housing.service.ts
+++ Frontend/src/app/services/housing.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map, Observable } from 'rxjs';
+import { IProperty } from '../property/IProperty.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -10,8 +12,18 @@ export class HousingService {
 
 constructor(private http: HttpClient) { }
 
-  getAllProperties(){
-    return this.http.get(this.path);
+  getAllProperties(): Observable<IProperty[]>{
+    return this.http.get(this.path).pipe(
+      map(data => {
+        const propertiesArray: Array<IProperty> = [];
+        for(const id in data){
+          if(data.hasOwnProperty(id)){
+            propertiesArray.push(data[id]);
+          }
+        }
+        return propertiesArray;
+      })
+    );
   }
 
 }
