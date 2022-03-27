8b4853654d2414312286ce3eaff67d0eef389821
Lordede <lukas.edm@web.de>
2022-03-27 17:11:05 +0200

    changes until v20 add dropdown link

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -15,6 +15,7 @@ import { UserRegisterComponent } from './user/user-register/user-register.compon
 import { UserLoginComponent } from './user/user-login/user-login.component';
 import { UserService } from './services/user.service';
 import { AlertifyService } from './services/alertify.service';
+import { AuthService } from './services/auth.service';
 
 const appRoutes: Routes = [
   {path: "add-property", component: AddPropertyComponent},
@@ -47,7 +48,8 @@ const appRoutes: Routes = [
   providers: [
     HousingService,
     UserService,
-    AlertifyService
+    AlertifyService,
+    AuthService
   ],
   bootstrap: [AppComponent]
 })
